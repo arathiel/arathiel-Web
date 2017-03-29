1c4b11d4e25ab78acb763700c2848a0910fb4c4d
Jonathan Fuentes <foujo@hotmail.com>
2017-03-29 16:35:55 +0200

    BeanAction

---
+++ src/beanAction/trait/consultation/Consult.java
@@ -0,0 +1,18 @@
+package beanAction.trait.consultation;
+
+import beanAction.ApplicationSupport;
+
+/**
+ * Classe d'actino pour la consultation de trait
+ * 
+ * @author Jonathan Fuentes
+ *
+ */
+public class Consult extends ApplicationSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/beanAction/trait/gestionnaire/Admin.java
@@ -0,0 +1,17 @@
+package beanAction.trait.gestionnaire;
+
+import beanAction.ApplicationSupport;
+
+/**
+ * Classe d'action pour l'administration de trait
+ * @author Jonathan Fuentes
+ *
+ */
+public class Admin extends ApplicationSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
