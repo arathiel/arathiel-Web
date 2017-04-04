5a496b6ef63695b53d1f2febbe0fde1563d9891e
Jonathan Fuentes <foujo@hotmail.com>
2017-04-04 11:05:22 +0200

    Lister et chercher trait

--- WebContent/js/trait/consult.js
+++ WebContent/js/trait/consult.js
@@ -1,31 +1,47 @@
-console.log('La JS se lance bien')
+window.onload = test;
+
+function test() {
+	console.log ('JS consult.js chargé')
+}
+
 /**
  * Application pour trait, regroupant les différents controlleurs
  */
 var app = angular.module('trait', []);
 
+
 /**
  * Mis en place du controller de saisie pour la recherche dynamique
  */
 app.controller('searchCtrl', function($scope, $http, $httpParamSerializer) {
-
-  	$scope.saisie 		= {'libSaisie':''};
-	$scope.reponse 		= "";
-	$scope.listeTrait	= "";
-    
+	//Initialisation de la variable
+	$scope.saisie 		= {'libelle':''};
+	
+	//Fonction pour la recherche dynamique
 	$scope.dynamicSearch = function() {
 
-	$scope.reponse = "";
-		
-	$http.post('http://localhost:8080/arathiel-Web/traitjson/searchTr.action', 
-				$httpParamSerializer($scope.saisie), 
-				{headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
-				.then(
-					function (response) {
-						$scope.status = response.status;
-			
-						// reponse devient un tableau d'objet
-						$scope.reponse = response.data.listeTrait;
+		//Initialisation des variables
+		$scope.reponse	 	= "";
+		$scope.listeTrait	= "";
+	
+	
+		//Requête + Retour
+		$http.post('http://localhost:8080/arathiel-Web/traitjson/searchTr.action', 
+					$httpParamSerializer($scope.saisie), 
+					{headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+					.then(
+							function (response) {
+								console.log('Dans la function success')
+						
+								//Initialisation de la variable response			
+								$scope.status = response.status;
+								console.log($scope.status)
+								
+								$scope.reponse = response.data;
+								console.log($scope.reponse)
+								
+								$scope.listeTrait = response.data.listTrait;
+								console.log($scope.listeTrait)
 
 					}, 
 					
@@ -35,10 +51,3 @@ app.controller('searchCtrl', function($scope, $http, $httpParamSerializer) {
 					});
      			}
 });// Fin searchCtrl 
-
-
-
-
-
-
-
