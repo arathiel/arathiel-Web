6b5f244c30ca2e2005180677189fe1b2a8881820
OllyB <tyno0809@gmail.com>
2017-04-05 11:14:25 +0200

    modif armurerieparam

--- src/util/armurerie/ArmurerieParam.java
+++ src/util/armurerie/ArmurerieParam.java
@@ -1,7 +1,7 @@
 package util.armurerie;
 
 public class ArmurerieParam {
-	
+
 	public static final String FACADE_SERVICE 	= "ejb:/arathiel-EJB/FacadeService!clientServeur.IFacadeService";
 	public static final String FORGE_SERVICE 	= "ejb:/arathiel-EJB/ForgeArme!clientServeur.IFabrique";
 
