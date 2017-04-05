05419b9ef2ff99af0a2c7e20bd24b2129505b0c5
OllyB <tyno0809@gmail.com>
2017-04-05 11:04:27 +0200

    modif Arme

--- src/entity/armurerie/Arme.java
+++ src/entity/armurerie/Arme.java
@@ -1,34 +1,55 @@
 package entity.armurerie;
 
 import java.io.Serializable;
+import java.util.Collection;
+
+import clientServeur.IArme;
+import entity.race_bonus_carac.race.Race;
+
 
 /**
  * Classe Arme permettant au joueur de s'équiper pour attaquer
  * @author OlivB
  *
  */
-public class Arme implements Serializable{
+public class Arme implements IArme, Serializable{
 
 
 	private static final long serialVersionUID = 1L;
 	
+	private int idArme;
 	private String nom;
-//	private Race race;
 	private int encombrement;
 	private int prix;
 	private String monnaie;
+	private Collection<Race> races;
 	
 //Constructeurs de la classe	
 	public Arme() {
 		super();
 	}
 	
+	public Arme(int idArme) {
+		super();
+		this.idArme = idArme;
+	}
+	
+	public Arme(int idArme, String nom) {
+		super();
+		this.idArme = idArme;
+		this.nom = nom;
+	}
+
 	public Arme(String nom, int encombrement, int prix, String monnaie) {
 		super();
 		this.nom = nom;
 		this.encombrement = encombrement;
 		this.prix = prix;
 		this.monnaie = monnaie;
+	}
+
+	public void setIdArme(int idArme) {
+		this.idArme = idArme;
 	}
 
 	//Getters et Setters
@@ -56,5 +77,27 @@ public class Arme implements Serializable{
 	public void setMonnaie(String monnaie) {
 		this.monnaie = monnaie;
 	}
+
+
+	@Override
+	public int getIdArme() {
+		return idArme;
+	}
+
+	@Override
+	public Collection<Race> getRaces() {
+		return races;
+	}
+
+	@Override
+	public void setRaces(Collection<Race> races) {
+		this.races = races;
+	}
+
+	@Override
+	public String toString() {
+		return "Arme [idArme=" + idArme + ", nom=" + nom + ", encombrement=" + encombrement + ", prix=" + prix
+				+ ", monnaie=" + monnaie + ", races=" + races + "]";
+	}	
 
 }
