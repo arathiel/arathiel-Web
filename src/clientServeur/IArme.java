a1d13b70516b5c661ceea4e9757376cbd88ffb1e
OllyB <tyno0809@gmail.com>
2017-04-05 11:14:06 +0200

    modif IArme

--- src/clientServeur/IArme.java
+++ src/clientServeur/IArme.java
@@ -11,17 +11,17 @@ import entity.race_bonus_carac.race.Race;
  */
 public interface IArme {
 
-	
-	public String getNom();
-	public void setNom(String nom);
-	public int getEncombrement();
-	public void setEncombrement(int encombrement);
-	public int getPrix();
-	public void setPrix(int prix);
-	public String getMonnaie();
-	public void setMonnaie(String monnaie);
-	public int getIdArme();
-	public Collection<Race> getRaces();
-	public void setRaces(Collection<Race> races);
-;
+
+	public String 				getNom();
+	public void 				setNom(String nom);
+	public int 					getEncombrement();
+	public void 				setEncombrement(int encombrement);
+	public int 					getPrix();
+	public void 				setPrix(int prix);
+	public String 				getMonnaie();
+	public void 				setMonnaie(String monnaie);
+	public int 					getIdArme();
+	public Collection<Race> 	getRaces();
+	public void 				setRaces(Collection<Race> races);
+	;
 }
