5e9f630e19eb154523e8cc121d2072c0dc0224fc
OllyB <tyno0809@gmail.com>
2017-04-10 16:26:43 +0200

    modif Armurerieparam

--- src/util/armurerie/ArmurerieParam.java
+++ src/util/armurerie/ArmurerieParam.java
@@ -2,7 +2,11 @@ package util.armurerie;
 
 public class ArmurerieParam {
 
-	public static final String FACADE_SERVICE 	= "ejb:/arathiel-EJB/FacadeService!clientServeur.IFacadeService";
-	public static final String FORGE_SERVICE 	= "ejb:/arathiel-EJB/ForgeArme!clientServeur.IForge";
+	public static final String 	FACADE_SERVICE 	= "ejb:/arathiel-EJB/FacadeService!clientServeur.IFacadeService";
+	public static final String 	FORGE_SERVICE 	= "ejb:/arathiel-EJB/ForgeArme!clientServeur.IForge";
+	public static final String 	destinationName = "/ConnectionFactory";
+	public static final String 	JMS_QUEUE_JNDI	 = "/jboss/exported/jms/queue/TestDdevilQueue"; 
+	public static final String 	JMS_USERNAME	 = "jmsuser";       
+	public static final String 	JMS_PASSWORD	 = "jmsuser@123";
 
 }
